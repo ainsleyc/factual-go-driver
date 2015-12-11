355251d8b7dbe4cfdaba9cf926eac76c90449efb
Ainsley Chong <ainsley.chong@gmail.com>
2015-12-11 15:19:26 -0800

    Added http

---
+++ http/http.go
@@ -0,0 +1,9 @@
+package factual
+
+func Get(url string) string {
+  return "GET"
+}
+
+func Post(url string) string {
+  return "POST"
+}
---
+++ http/http_test.go
@@ -0,0 +1,13 @@
+package factual
+
+import (
+  "testing"
+)
+
+func TestGet(t *testing.T) {
+  Get("BLAH")
+}
+
+func TestPost(t *testing.T) {
+  Get("BLAH")
+}
