a68af3dbf8bd0320cac5360e93a9f55f987f10d7
Ainsley Chong <ainsley.chong@gmail.com>
2015-12-28 08:51:59 -0800

    Added GeoCircle integration test

--- integration_test.go
+++ integration_test.go
@@ -119,3 +119,17 @@ func TestGet_ReadWithLogicalFilter_ShouldReturnResults(t *testing.T) {
 
 	testGet(t, path, params)
 }
+
+// /t/places-us?geo={"$circle":{"$center":[34.06021,-118.41828],"$meters":5000}
+func TestGet_ReadWithGeoCircle_ShouldReturnResults(t *testing.T) {
+	path := "/t/places-us"
+	params := url.Values{}
+	geo, _ := factual.NewGeoCircle(
+    float64(34.06021),
+    float64(-118.41828),
+    5000,
+	).MarshalJSON()
+	params.Set("geo", string(geo))
+
+	testGet(t, path, params)
+}
