5e63ad8a4a0f7ea2b413c3ae9298819618cfcfcd
Drew Wells <drew.wells00@gmail.com>
2016-07-06 12:09:28 -0500

    implement appengine http client and example

--- factual.go
+++ factual.go
@@ -1,21 +1,40 @@
 package factual
 
 import (
+	"net/http"
+
 	"github.com/garyburd/go-oauth/oauth"
+	"golang.org/x/net/context"
 )
 
 type Client struct {
 	Oauth   oauth.Client
 	BaseUri string
+	ctx     context.Context
 }
 
 const defaultUri = "https://api.factual.com"
 
-func NewClient(key string, secret string) Client {
+// NewClient accepts a client ID and client secret and returns
+// a factual client. It accepts variable number of functional
+// options for configuring the client.
+func NewClient(key string, secret string, opts ...Option) Client {
 	creds := oauth.Credentials{key, secret}
 	oauthClient := oauth.Client{
 		Credentials:     creds,
 		SignatureMethod: oauth.HMACSHA1,
 	}
-	return Client{oauthClient, defaultUri}
+	c := &Client{
+		Oauth:   oauthClient,
+		BaseUri: defaultUri,
+	}
+	for _, opt := range opts {
+		opt(c)
+	}
+	return *c
 }
+
+// Option are functional options for configuring a Client
+type Option func(c *Client) error
+
+var getClient func(ctx context.Context) *http.Client
